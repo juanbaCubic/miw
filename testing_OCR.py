19dca21842e6b34987359e17dfae4f5c9f7dc6c7
Juan Bautista Tomás Gabarrón <juanba.tomas@peranoid.com>
2022-11-15 08:55:42 +0100

    testing_OCR.py modified

--- testing_OCR.py
+++ testing_OCR.py
@@ -6,11 +6,10 @@ url = "https://miw.iamai.es/textract_analysis"
 
 arrayFiles = ['Factura_01.pdf', 'Factura_02.pdf', 'Factura_03.pdf', 'Factura_04.pdf', 'Factura_05.pdf', 'Factura_06.pdf', 'Factura_07.pdf', 'Factura_08.pdf', 'Factura_09.pdf', 'Factura_10.pdf', 'Factura_108585.pdf', 'Factura_11.pdf', 'Factura_110892.pdf', 'Factura_113022.pdf', 'Factura_115660.pdf', 'Factura_117190.pdf', 'Factura_119410.pdf', 'Factura_12.pdf', 'Factura_120976.pdf', 'Factura_124004.pdf', 'Factura_126076.pdf', 'Factura_127920.pdf', 'Factura_129939.pdf', 'Factura_13.pdf', 'Factura_134456.pdf', 'Factura_138221.pdf', 'Factura_14.pdf', 'Factura_141461.pdf', 'Factura_15.pdf', 'Factura_16.pdf', 'Factura_17.pdf', 'Factura_18.pdf', 'Factura_19.pdf', 'Factura_20.pdf', 'Factura_21.pdf', 'Factura_22.pdf', 'Factura_23.pdf', 'Factura_24.pdf', 'Factura_25.pdf', 'Factura_26.pdf', 'Factura_27.pdf', 'Factura_28.pdf', 'Factura_29.pdf', 'Factura_30.pdf', 'Factura_31.pdf', 'Factura_32.pdf', 'Factura_33.pdf', 'Factura_34.pdf', 'Factura_35.pdf', 'Factura_36.pdf', 'Factura_37.pdf', 'Factura_38.pdf', 'Factura_39.pdf', 'Factura_40.pdf', 'Factura_41.pdf', 'Factura_42.pdf', 'Factura_43.pdf', 'Factura_44.pdf', 'Factura_45.pdf', 'Factura_46.pdf', 'Factura_47.pdf', 'Factura_48.pdf', 'Factura_49.pdf', 'Factura_50.pdf', 'Factura_51.pdf']
 
-arrayFiles2=['Factura_01.pdf', 'Factura_02.pdf']
 jobs = []
 
 # 1. Mandamos las facturas para generar los jobs asíncronos,, metiéndolos en un array.
-for file in arrayFiles2:
+for file in arrayFiles:
     print("Processing file: "+str(file))
     payload = json.dumps({
       "s3_url": "s3://textractinvoiceanalysisocr/" + file
